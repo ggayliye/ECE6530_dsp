daa0a7f43ecdb108f971cf67d9e501afb04d3564
ggayliye <122452379+ggayliye@users.noreply.github.com>
2024-08-27 02:13:54 -0600

    Create draft .m files for ex1.

---
+++ module1/matlab/Q3.m
@@ -0,0 +1,10 @@
+% ========================================================================== 
+% ECE 5530 / ECE 6530 Matlab Ex 1: Introduction to Discrete-Time Signals
+% Team: Kyle G. Gayliyev, Skylar Stockham
+% Aug 27th, 2024
+% ========================================================================== 
+
+% Assignment 1. Question 3.
+
+
+
